a374f80e829bc87c754d21c7b40acd73f6994c9c
EY1028 <ey221028@gmail.com>
2025-10-01 18:08:44 +0900

    18:10

---
+++ LEE/index.js
@@ -0,0 +1,16 @@
+const box = document.querySelector(".background-image");
+
+const originalWidth = "1920px";
+const originalHeight = "953px";
+const reducedWidth = "1200px";
+const reducedHeight = "360px";
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    box.style.width = reducedWidth;
+    box.style.height = reducedHeight;
+  } else {
+    box.style.width = originalWidth;
+    box.style.height = originalHeight;
+  }
+});
