0ca34ed01bfb91d96f80e0c3ba895c4e44347951
jungwoo Son <thswjddn0812@gmail.com>
2025-10-02 15:19:01 +0900

    15:18

--- LEE/index.js
+++ LEE/index.js
@@ -9,11 +9,13 @@ const reducedHeight = "40%";
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
+    box.style.animationPlayState = "paused";
     box.style.width = reducedWidth;
     box.style.height = reducedHeight;
     SmvText.style.opacity = "0";
     LscrollNavWrap.style.opacity = "0";
   } else {
+    box.style.animationPlayState = "running";
     box.style.width = originalWidth;
     box.style.height = originalHeight;
     SmvText.style.opacity = "1";
