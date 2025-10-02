e8fc04321c9da1362555828ebfd1e942163f6434
EY1028 <ey221028@gmail.com>
2025-10-02 15:18:45 +0900

    18:10

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 window.addEventListener("scroll", () => {
-  const scrollY = window.scrollY;
-  const imgbox = document.querySelector(".heroimgbox");
-
-  // 스크롤 0~200px까지 이미지 크기 줄이기
-  if (scrollY != 0) {
-    imgbox.style.transform = "scale(0.62, 0.34)";
-  } else {
-    imgbox.style.transform = "scale(1)";
-  }
+  // const scrollY = window.scrollY;
+  // const imgbox = document.querySelector(".heroimgbox");
+  // // 스크롤 0~200px까지 이미지 크기 줄이기
+  // if (scrollY != 0) {
+  //   imgbox.style.transform = "scale(0.62, 0.34)";
+  // } else {
+  //   imgbox.style.transform = "scale(1)";
+  // }
 });
